5bd6b041d598fdce17f3a43a40f10066114c79ff
shyam_sundhar <152371963+bssshyamsundhar@users.noreply.github.com>
2025-07-09 19:24:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,28 +29,26 @@ from flask import Flask, request, redirect, url_for
 import os
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'uploads/'  # Replace with your actual folder path
+app.config['UPLOAD_FOLDER'] = 'uploads/' 
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    # Check if the post request has the file part
     if 'file' not in request.files:
-        return 'No file part', 400  # Return a 400 Bad Request error code
+        return 'No file part', 400
     
     file = request.files['file']
     
-    # If user does not select file, browser also submit an empty part without filename
     if file.filename == '':
-        return 'No selected file', 400  # Return a 400 Bad Request error code
+        return 'No selected file', 400
     
     if file:
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         
         try:
             file.save(filepath)
-            return f'File successfully uploaded to {filepath}', 200  # Return a 200 OK status code
+            return f'File successfully uploaded to {filepath}', 200 
         except Exception as e:
-            return str(e), 500  # Return a 500 Internal Server Error code with the exception message
+            return str(e), 500 
 
 if __name__ == '__main__':
     app.run(debug=True)
